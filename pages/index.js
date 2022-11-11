1baf07d8eb7f3cb90d712bd44b5b57218cde2b47
Tsiniloiv <jon-gisli93@hotmail.com>
2022-11-11 12:51:10 +0000

    brute force time

--- pages/index.js
+++ pages/index.js
@@ -369,7 +369,7 @@ export default function Home() {
       return error;
     }
     if(searchResults && !error) {
-      return "Data: " + JSON.stringify(data);
+      return searchResults;
     }
   }
 
@@ -388,6 +388,13 @@ export default function Home() {
               <input class="m-1 p-1" type="text" id="searchString" name="searchString" />
               <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Leita</button>
             </form>
+        </div>
+
+        <div>
+          searchString: {searchString}
+          validating: {JSON.stringify(isValidating)}
+          data: {JSON.stringify(data)}
+          searchResults: {JSON.stringify(searchResults)}
         </div>
 
         <div class="container flex flex-row flex-wrap h-full pt-4">
